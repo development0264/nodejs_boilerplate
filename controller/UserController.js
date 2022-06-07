2968c5da62426c56ce82b0398f86b4fe01d8e397
Development <development0264@gmail.com>
2022-06-07 12:44:46 +0530

    Implemented JWT auth

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,15 +1,55 @@
 const User = require("../models/User.js");
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken')
 
-// CREATE
+// CREATE || REGISTER
 const create = async (req, res) => {
-    const { name, email } = req.body
+    const { name, email, password } = req.body
     try {
-        if(name && email){
-            User.create({ name: name, email: email }, function (err) {
+        if (name && email && password) {
+            // encrypt the password using bcrypt.hash function with salt Rounds = 10
+            const hashedPwd = await bcrypt.hash(password, 10);
+
+            User.create({ name: name, email: email, password: hashedPwd }, function (err) {
                 if (err) return res.status(400).json({ message: err.message })
-                else res.status(201).json({ message: "User Created Successfully" })
+                else res.status(201).json({ message: `User ${name} Created Successfully` })
             })
-        } else res.status(400).json({ message: "Somethings wrong !!" })
+        } else res.status(400).json({ message: "name, email and password required" })
+    } catch (error) {
+        console.log("---error----", error);
+    }
+}
+
+// AUTH
+const handleLogin = async (req, res) => {
+    const { email, password } = req.body
+    try {
+        if (email && password) {
+            // find user with given email
+            User.find({ 'email': email }, async function (err, foundUser) {
+                const user = foundUser[0]
+                if (err) return res.status(400).json({ message: err.message })
+                else if (!user) return res.status(401).json({ message: `No user found with ${email}` })
+                else {
+                    // evaluate password, matching encrypted and entered password
+                    const match = await bcrypt.compare(password, user.password); // If true password = user.password
+                    if (match) {
+                        // creating JWT here with user's name info and 10min expiration time.
+                        const accessToken = jwt.sign(
+                            { "name": user.name },
+                            process.env.ACCESS_TOKEN_SECRET,
+                            { expiresIn: '10m' }
+                        )
+                        res.status(200).json({
+                            'success': `${user.name} login success`,
+                            'accessToken': accessToken
+                        });
+                    } else {
+                        res.status(401).json({ message: `Invalid email or password` });
+                    }
+                }
+            });
+        } else res.status(400).json({ message: "email and password required" })
     } catch (error) {
         console.log("---error----", error);
     }
@@ -19,7 +59,7 @@ const create = async (req, res) => {
 const getAll = async (req, res) => {
     try {
         const users = await User.find({}).select('-__v');
-        res.status(200).json({ message: `Total available users ${users.length}`, users: users });
+        return res.status(200).json({ message: `Total available users ${users.length}`, users: users });
     } catch (error) {
         console.log("Errr ===> ", error);
     }
@@ -66,5 +106,6 @@ module.exports = {
     getAll,
     create,
     deleteUser,
-    updateUser
+    updateUser,
+    handleLogin
 }
